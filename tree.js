654f5bc5d4de658373039554c4bf44a02eaa1bc2
Gayathri Sriram <gayathriammu@Gayathris-MacBook-Air.local>
2020-10-10 21:07:15 +0530

    Images with I capitals

--- tree.js
+++ tree.js
@@ -4,7 +4,7 @@ class Tree{
             isStatic: true
         }
         this.body = Bodies.rectangle(x,y,200,400,options);
-        this.image = loadImage("images/tree.png");
+        this.image = loadImage("Images/tree.png");
         this.width = 400;
         this.height = 400;
         World.add(world, this.body);
